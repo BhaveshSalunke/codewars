61960869cfb4222a704db433eb613bd5a219d980
neo <bhaveshsalunke14@gmail.com>
2021-02-04 16:44:30 +0530

    initial commit

---
+++ src/main/java/com/codewars/kata/EqualSidesOfAnArray.java
@@ -0,0 +1,23 @@
+package main.java.com.codewars.kata;
+
+public class EqualSidesOfAnArray {
+
+
+    public static int findEvenIndex(int[] arr) {
+        int arrSize = arr.length;
+        for (int i = 0; i < arrSize; i++) {
+
+            int sumRight = 0;
+            int sumLeft = 0;
+
+            for (int j = arrSize - 1; j > i; j--) sumRight += arr[j];
+
+            for (int k = 0; k < i; k++) sumLeft += arr[k];
+
+            if (sumRight == sumLeft) return i;
+
+        }
+
+        return -1;
+    }
+}
