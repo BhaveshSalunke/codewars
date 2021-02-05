497c4c59ea10463c43171acba58dabfaed0dfc35
neo <bhaveshsalunke14@gmail.com>
2021-02-05 13:39:03 +0530

    GiveMeDiamond Problem added

---
+++ src/main/java/com/codewars/kata/solution/JadenCasingString/JadenCasingStringTest.java
@@ -0,0 +1,30 @@
+package main.java.com.codewars.kata.solution.JadenCasingString;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import main.java.com.codewars.kata.solution.JadenCasingString.JadenCasingString;
+import org.junit.Test;
+
+public class JadenCasingStringTest {
+
+
+    JadenCasingString jadenCase = new JadenCasingString();
+
+    @Test
+    public void test() {
+        assertEquals("toJadenCase doesn't return a valide JadenCase String! try again please :)", "Most Trees Are Blue", jadenCase.toJadenCase("most trees are blue"));
+    }
+
+    @Test
+    public void testNullArg() {
+        assertNull("Must return null when the arg is null", jadenCase.toJadenCase(null));
+    }
+
+    @Test
+    public void testEmptyArg() {
+        assertNull("Must return null when the arg is empty string", jadenCase.toJadenCase(""));
+    }
+
+}
