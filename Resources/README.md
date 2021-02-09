7b4c5f2e4dc7f401b9b8fbda67a7244ec9bcc3fc
Mrunmayi Dharmadhikari <mrunmayi016@gmail.com>
2021-02-09 14:35:36 -0500

    Resources added 1)README.md file added. 2)Homework repository added.

---
+++ Resources/README.md
@@ -0,0 +1,8 @@
+#Homework
+
+Create a file for each and every day task to perform after meeting
+
+##format
+###//topic of discussion
+###//performed tasks
+###//tasks to be performed
--- src/main/java/com/codewars/kata/Main.java
+++ src/main/java/com/codewars/kata/Main.java
@@ -1,9 +1,7 @@
 package main.java.com.codewars.kata;
 
-import main.java.com.codewars.kata.solution.HighestScoringWord.HighestScoringWord;
-
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("Hello, Mrunmayi here!");
 
 
