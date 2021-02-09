e9b35f39770f44efd120ca5e37efe06b1a7b6633
neo <bhaveshsalunke14@gmail.com>
2021-02-09 19:26:25 +0530

    MultiplierOf3Or5 added

---
+++ src/main/java/com/codewars/kata/solution/MultiplierOf3Or5/MultiplierOf3Or5Test.java
@@ -0,0 +1,13 @@
+package main.java.com.codewars.kata.solution.MultiplierOf3Or5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MultiplierOf3Or5Test {
+
+    @Test
+    public void solution() {
+        assertEquals(23, new MultiplierOf3Or5().solution(10));
+    }
+}
