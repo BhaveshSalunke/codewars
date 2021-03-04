f3d2bdb30a612a94dc1aa473fc3d9cebadc74976
neo <bhaveshsalunke14@gmail.com>
2021-03-04 11:43:51 +0530

    StringyString.java added

--- src/main/java/com/codewars/kata/solution/StringyString/StringyString.java
+++ src/main/java/com/codewars/kata/solution/StringyString/StringyString.java
@@ -11,7 +11,6 @@ public class StringyString {
 
             if (flag) output = output.concat("1");
             if (!flag) output = output.concat("0");
-
             flag = !flag;
         }
         return output;
